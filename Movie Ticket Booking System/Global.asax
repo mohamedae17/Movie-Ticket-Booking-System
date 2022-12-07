0f220be729d2e403dcfe41d577f78e342d86e703
Mohamed Afify <mohamed.ae1717@gmail.com>
2022-12-07 10:00:04 +0200

    Add project files.

---
+++ Movie Ticket Booking System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Movie_Ticket_Booking_System.MvcApplication" Language="C#" %>
---
+++ Movie Ticket Booking System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movie_Ticket_Booking_System.Startup))]
+namespace Movie_Ticket_Booking_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
