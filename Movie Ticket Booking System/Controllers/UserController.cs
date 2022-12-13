56ca8799392061b0725ac593ae230bda07d26c01
Mohamed Afify <mohamed.ae1717@gmail.com>
2022-12-13 16:44:06 +0200

    View cinema and movies Details of Movie

---
+++ Movie Ticket Booking System/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using Movie_Ticket_Booking_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Movie_Ticket_Booking_System.ViewModels;
+using System.Net;
+
+namespace Movie_Ticket_Booking_System.Controllers
+{
+    public class UserController : Controller
+    {
+        private ApplicationDbContext _context;
+        public UserController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: User
+        public ActionResult Index()
+        {
+            List<MoviesInCinema> list = new List<MoviesInCinema>();
+            List<Cinema> cinemas = _context.Cinemas.ToList();
+            foreach (var cinema in cinemas)
+            {
+                var item = new MoviesInCinema();
+                item.cinema = cinema.CinemaName;
+                List<MovieDetails> Movies = new List<MovieDetails>();
+                var Halls = _context.Halls.Where(h => h.CinemaId == cinema.Id).ToList();
+                List<Show> shows = new List<Show>();
+                foreach (var Hall in Halls)
+                {
+                    var ShowList = _context.Shows.Where(s => s.HallId == Hall.Id).DefaultIfEmpty().Single();
+                    shows.Add(ShowList);
+                }
+                foreach (var show in shows)
+                {
+                    if(show != null)
+                    { 
+                        var MovieList = _context.MovieDetails.Where(m => m.Id == show.MovieId).FirstOrDefault();
+                        Movies.Add(MovieList);
+                    }
+                }
+                item.MovieDetails = Movies;
+                list.Add(item);
+            }
+            return View(list);
+        }
+        // GET: MovieDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MovieDetails movieDetails = _context.MovieDetails.Find(id);
+            if (movieDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movieDetails);
+        }
+    }
+}
---
+++ Movie Ticket Booking System/ViewModels/MoviesInCinema.cs
@@ -0,0 +1,14 @@
+﻿using Movie_Ticket_Booking_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie_Ticket_Booking_System.ViewModels
+{
+    public class MoviesInCinema
+    {
+        public string cinema { get; set; }
+        public List<MovieDetails> MovieDetails { get; set; }
+    }
+}
